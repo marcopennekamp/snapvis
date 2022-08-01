1cbbe45bc629855728675d3757751bfaacc7180e
marcopennekamp <marco.pennekamp96@gmail.com>
2022-08-01 14:46:40 +0200

    Move common calculator snapshot test functions into their own object.
    - This functionality will be used by other tests.

---
+++ snapvis-plugin/src/test/kotlin/snapvis/SampleCalculatorSnapshot.kt
@@ -0,0 +1,59 @@
+package snapvis
+
+import com.intellij.openapi.project.Project
+import snapvis.extractors.Extractors
+import snapvis.metrics.CallMetrics
+import snapvis.metrics.MethodCallTime
+import snapvis.metrics.MetricsService
+import snapvis.util.Nanoseconds
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+/**
+ * [SampleCalculatorSnapshot] helps several tests extract and verify the sample calculator snapshot.
+ */
+object SampleCalculatorSnapshot {
+    fun extract(): CallMetrics {
+        val fileName = javaClass.classLoader.getResource("calculator_snapshot.jfr")?.file
+        assertNotNull(fileName)
+        val extractor = Extractors.forExtension("jfr")
+        assertNotNull(extractor)
+        return extractor.extract(fileName)
+    }
+
+    fun verifyMetrics(metrics: CallMetrics) {
+        // Note that these nanosecond timings aren't necessarily representative of the calculator's actual run time
+        // because the sampling rate of the snapshot is quite low. What the test should ensure is that these method
+        // call instances are discovered in the snapshot and the execution times are consistently calculated.
+        val callTime1 = metrics.get("calculator.Tokenizer").get(61, "toDoubleOrNull")
+        assertNotNull(callTime1)
+        assertEquals(MethodCallTime("toDoubleOrNull", Nanoseconds(2581300)), callTime1)
+
+        val callTime2 = metrics.get("calculator.EvaluatorKt").get(37, "addLast")
+        assertNotNull(callTime2)
+        assertEquals(MethodCallTime("addLast", Nanoseconds(253140)), callTime2)
+
+        val callTime3 = metrics.get("calculator.CalculatorKt").get(13, "calculate")
+        assertNotNull(callTime3)
+        assertEquals(MethodCallTime("calculate", Nanoseconds(138848335)), callTime3)
+    }
+
+    /**
+     * Sets up the calculator snapshot in [MetricsService].
+     */
+    fun setUp(project: Project) {
+        val metricsService = project.getService(MetricsService::class.java)
+        assertNotNull(metricsService)
+        val metrics = extract()
+        metricsService.callMetrics = metrics
+    }
+
+    /**
+     * Removes the calculator snapshot metrics from [MetricsService].
+     */
+    fun tearDown(project: Project) {
+        val metricsService = project.getService(MetricsService::class.java)
+        assertNotNull(metricsService)
+        metricsService.callMetrics = null
+    }
+}
